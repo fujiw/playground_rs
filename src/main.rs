a4803236dfdc43f8ae9d324e62fa2d7ab8ae002c
fujiw <fujiw10@gmail.com>
2020-10-29 00:12:33 +0900

    async block

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::time::Duration;
+use tokio::time::delay_for;
 
 #[tokio::main]
 async fn main() {
     for i in 0..10 {
         println!("{}", i);
-        tokio::time::delay_for(Duration::from_secs(1)).await;
+        delay_for(Duration::from_secs(1)).await;
     }
+
+    let _async_block = async {
+        delay_for(Duration::from_secs(1)).await;
+        0
+    };
+
+    let s = "hoge".to_string();
+    let _move_block = async move {
+        println!("{}", s);
+    };
+
+    //println!("{}", s);
 }
