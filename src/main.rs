80434927966c3ffc108b41fad2358c7c7e8e8dbd
fujiw <fujiw10@gmail.com>
2020-10-29 00:04:13 +0900

    async/await

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
+    for i in 0..10 {
+        println!("{}", i);
+        tokio::time::delay_for(Duration::from_secs(1)).await;
+    }
 }
